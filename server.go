46710aa2792b80b4b4cbc1dd1efb533cf7005fb3
mo.taheri <mo.taheri@mahsan.co>
2020-11-27 21:20:07 +0330

    conflict workspace

--- server.go
+++ server.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tmohammad78/food-api/controllers"
+	"github.com/tmohammad78/food-api/middlewares"
 	"github.com/tmohammad78/food-api/services"
+	"io"
+	"os"
 )
 
 var (
@@ -11,8 +14,15 @@ var (
 	foodController controllers.FoodController = controllers.New(foodService)
 )
 
+/** method for save logger files */
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+	server := gin.New()
+	server.Use(gin.Recovery(), middlewares.Logger())
 	server.GET("/food", func(context *gin.Context) {
 		context.JSON(200, foodController.GetFood())
 	})
